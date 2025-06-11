b15576611c790d794f6ace235c629952f3e54fed
adamparjun <adamparjunsiregar2@gmail.com>
2025-06-11 23:52:10 +0700

    Delete contract.js

--- contract.js
+++
@@ -1,9 +0,0 @@
-async function joinBet(roomId) {
-  console.log("Joining bet for room:", roomId);
-  // Dummy: simulate token approve & bet call
-  return new Promise(resolve => setTimeout(resolve, 1000));
-}
-
-async function payout(roomId) {
-  console.log("Payout STT for room:", roomId);
-}
