46c6fd8f3f61e3464b39b3b1d4e7f926c187d562
adamparjun <adamparjunsiregar2@gmail.com>
2025-06-11 22:52:06 +0700

    Add files via upload

---
+++ gameEngine.js
@@ -0,0 +1,7 @@
+function simulateBallPhysics(angle, power) {
+  console.log("Simulating shot", angle, power);
+}
+
+function animateToShot(angle, power, positions) {
+  console.log("Animate shot to:", positions);
+}
