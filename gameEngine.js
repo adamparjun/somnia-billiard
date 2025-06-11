77df073450acf3452cf702f9ea0d9f0954a078af
adamparjun <adamparjunsiregar2@gmail.com>
2025-06-11 23:52:00 +0700

    Delete gameEngine.js

--- gameEngine.js
+++
@@ -1,7 +0,0 @@
-function simulateBallPhysics(angle, power) {
-  console.log("Simulating shot", angle, power);
-}
-
-function animateToShot(angle, power, positions) {
-  console.log("Animate shot to:", positions);
-}
